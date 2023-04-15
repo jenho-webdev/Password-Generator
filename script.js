00b9290c17d36a7b74a510170df302e5ca7724de
Jen Ho <jenny.hofun@gmail.com>
2023-04-15 11:16:01 -0700

    create logic,getting user input and generate password.

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Generate Password per User's criteria.
+function generatePassword() 
+{
+  //declear arrays
+  var specialChar = '\!,\",\#,\$,\%,\&,\',\(,\),\*,\+,\,,\-,\.,\,\:,\;,\<,\=,\>,\?,\@,\[,\\,\],\^,\_,\`,\{,\|,\},\~';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZz';
+  var lowerCase = 'abcdefghijklmnopqrstuvwxy';
+  var numeric = '0123456789';
+  var charactersLength = prompt("How many characters you want to have in your password?(8-128 characters)");
+  var finalCharacters = '';
+  var result = '';
+
+  If(confirm("Do you want uppercase in your passowrd?"))
+    {
+      finalCharacters = finalCharacters + upperCase;
+    }
+    else if(confirm("Do you want lowercase in your passowrd?")
+    {
+      finalCharacters = finalCharacters + lowerCase;
+
+    }
+    else if(confirm("Do you want numeric charaters in your passowrd?")
+    {
+      finalCharacters = finalCharacters + numeric;
+    }
+
+  if(charactersLength <= 128 && charactersLength >= 8 )
+  {
+    for (let step = 0; step < charactersLength; step++)
+    { 
+      result = result + finalCharacters[step];
+    }
+    
+  }
+  return result;
+}
